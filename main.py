c91146b2347933a4b39cf04b3b58b092dd96c384
Zoe8888 <zoeerispe@gmail.com>
2021-04-15 16:03:44 +0200

    operators exercise

---
+++ main.py
@@ -0,0 +1,9 @@
+h = float(input("insert height value in cm: "))
+b = float(input("insert base value in cm: "))
+area = int((h*b)/2)
+print("area of triangle is: ", bin(area), "cm^")
+
+my_numbers = [2, 56, 12, 67, 1000, 32, 89, 29, 44, 39, 200, 11, 21]
+print("List of numbers: 2, 56, 12, 67, 1000, 32, 89, 29, 44, 39, 200, 11, 21")
+print("The smallest number in the list is: ", min(my_numbers))
+print("The largest number in the list is: ", max(my_numbers))
